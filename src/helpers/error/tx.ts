d0124f79ecef85e8704bbaa5ed209caeb895ce4f
Golddy Dev <firingdev@gmail.com>
2025-01-12 00:09:00 +0800

    refactor testing

--- src/helpers/error/tx.ts
+++ src/helpers/error/tx.ts
@@ -12,13 +12,13 @@ type HandleableResult<E extends ErrType> = {
   complete: () => Promise<Result<SuccessResult, E>>;
 };
 
+const halfArray = <T>(array: T[]): T[] =>
+  array.slice(0, Math.floor(array.length / 2));
+
 const mayFailTransaction = (
   txBuilder: TxBuilder,
   changeAddress: Address,
-  spareUtxos: TxInput[],
-  customValidationLogger: (msg: string) => void = (msg: string) => {
-    console.error("Validation log:", msg);
-  }
+  spareUtxos: TxInput[]
 ): HandleableResult<Error | BuildTxError> => {
   const createHandleable = (
     handler: (e: Error) => void
@@ -31,48 +31,66 @@ const mayFailTransaction = (
         const logs: string[] = [];
         const logger: UplcLogger = {
           ...makeBasicUplcLogger(),
-          logPrint: (msg: string) => {
-            logs.push(msg);
-            customValidationLogger(msg);
-          },
+          logPrint: (msg: string) => logs.push(msg),
         };
-        if (logger.reset) logger.reset("validate");
+        if (logger.reset) logger.reset("build");
         try {
-          const tx = await txBuilder.build({
+          const tx = await txBuilder.buildUnsafe({
             changeAddress,
             spareUtxos,
             logOptions: logger,
+            throwBuildPhaseScriptErrors: false,
           });
-          return Ok({ tx, dump: tx.dump() });
-        } catch (txError) {
-          if (logs.length == 0) {
-            /// when the error is not related to Tx Validation
-            const txBuildError = new Error(
-              `Tx Build error: ${convertError(txError)}`
+          if (tx.hasValidationError) {
+            const txValidationError = BuildTxError.fromError(
+              new Error(
+                convertError(tx.hasValidationError) +
+                  "\nValidation logs:" +
+                  halfArray(logs).map((log) => "\nLog: " + log)
+              ),
+              tx
             );
-            handler(txBuildError);
-            return Err(txBuildError);
-          }
-          try {
-            const failedTx = await txBuilder.buildUnsafe({
-              changeAddress,
-              spareUtxos,
-              throwBuildPhaseScriptErrors: false,
-            });
-            const txValidationError = new Error(
-              convertError(txError) +
-                "\nValidation logs:" +
-                logs.map((log) => "\nLog: " + log)
-            );
-            handler(BuildTxError.fromError(txValidationError, failedTx));
+            handler(txValidationError);
             return Err(txValidationError);
-          } catch (unexpectedError) {
-            const txError = new Error(
-              `Unexpected Error: ${convertError(unexpectedError)}`
-            );
-            return Err(txError);
           }
+          return Ok({ tx, dump: tx.dump() });
+        } catch (buildError) {
+          const txError = new Error(
+            `Tx Build Error: ${convertError(buildError)}`
+          );
+          handler(txError);
+          return Err(txError);
         }
+        // catch (txError) {
+        //   if (logs.length == 0) {
+        //     /// when the error is not related to Tx Validation
+        //     const txBuildError = new Error(
+        //       `Tx Build error: ${convertError(txError)}`
+        //     );
+        //     handler(txBuildError);
+        //     return Err(txBuildError);
+        //   }
+        //   try {
+        //     const failedTx = await txBuilder.buildUnsafe({
+        //       changeAddress,
+        //       spareUtxos,
+        //       throwBuildPhaseScriptErrors: false,
+        //     });
+        //     const txValidationError = new Error(
+        //       convertError(txError) +
+        //         "\nValidation logs:" +
+        //         logs.map((log) => "\nLog: " + log)
+        //     );
+        //     handler(BuildTxError.fromError(txValidationError, failedTx));
+        //     return Err(txValidationError);
+        //   } catch (unexpectedError) {
+        //     const txError = new Error(
+        //       `Unexpected Error: ${convertError(unexpectedError)}`
+        //     );
+        //     handler(txError);
+        //     return Err(txError);
+        //   }
+        // }
       },
     };
   };
